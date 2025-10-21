6190b7fbd1fc50e9165b4d2eb65118463829719c
Per Olsen <persteen22@gmail.com>
2025-10-21 17:03:02 +0200

    Set Debug to False

--- quickstartproject/settings.py
+++ quickstartproject/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '1234567890'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
